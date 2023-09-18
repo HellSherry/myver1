f83c69be9800e1957aab163a160e13b10dec86f3
HellSherry <114070772+HellSherry@users.noreply.github.com>
2023-09-18 19:25:06 +0800

    Add files via upload

---
+++ api/ccc.js
@@ -0,0 +1,24 @@
+module.exports = (req, res) => {
+  const { name = 'World' } = req.query
+  const https = require('https');
+
+  https.get('https://xiaoapi.cn/API/yy_sq.php?msg='+name+'&type=json&n=1', (response) => {
+      let todo = '';
+  
+      // called when a data chunk is received.
+      response.on('data', (chunk) => {
+          todo += chunk;
+      });
+  
+      // called when the complete response is received.
+      response.on('end', () => {
+        res.send(JSON.parse(todo));
+      });
+  
+  }).on("error", (error) => {
+    res.send("Error: " + error.message);
+  });
+  
+    // const { name = 'World' } = req.query
+   
+  }
